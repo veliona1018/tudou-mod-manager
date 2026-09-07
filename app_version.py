eb34f77cdecec9b53be856eed702e8644c76b5e1
veliona1018 <1114571332@qq.com>
2026-09-07 21:37:13 +0800

    Add Gitee update source support

--- app_version.py
+++ app_version.py
@@ -1,4 +1,5 @@
 """Application version and public update source."""
 
-APP_VERSION = "0.2"
+APP_VERSION = "0.32"
 UPDATE_REPOSITORY = "veliona1018/tudou-mod-manager"
+GITEE_REPOSITORY = "Suzuran_1009/tudou-mod-manager"
